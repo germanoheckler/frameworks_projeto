cc2b0707ecb668287863422260dd19fcbc201401
Germano Heckler <gheckler@gmail.com>
2023-08-01 18:59:23 -0400

    teste

---
+++ resources/views/login.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <title>Login</title>
+    <body>
+        <label>Usuario</label>
+        <input type="text" placeholder="Digite seu usuario">
+
+        <label>Senha</label>
+        <input type="password" placeholder="Digite sua senha">
+    </body>
+</html>
